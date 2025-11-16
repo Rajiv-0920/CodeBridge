faa5902df8e2af08ea18000dbcb6e01111b73897
Rajiv <kumarrajiv0920@gmail.com>
2025-11-16 20:05:21 +0530

    Adding validation check in db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import 'dotenv/config'
 
 export const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error(`Mongo URI is not defined in environment variable`)
+    }
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Mongodb Connected')
   } catch (error) {
