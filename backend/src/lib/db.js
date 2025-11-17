f5b04d404d9cd35088cddea797a8ab3932ab7d94
Rajiv <kumarrajiv0920@gmail.com>
2025-11-17 16:08:58 +0530

    Change db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -12,7 +12,7 @@ export const connectDB = async () => {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI)
+    const db = await mongoose.connect(process.env.MONGO_URI)
     connection.isConnected = db.connections[0].readyState
     console.log('Database connected successfully')
   } catch (error) {
